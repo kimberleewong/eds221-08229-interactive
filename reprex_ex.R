506fd228486645f7a12522a9d92f0bd589a61d6f
kimberleewong <kjwong02@ucla.edu>
2024-08-22 13:41:16 -0700

    reprex ex

---
+++ reprex_ex.R
@@ -0,0 +1,23 @@
+library(reprex)
+library(palmerpenguins)
+library(tidyverse)
+
+ggplot(data = penguins,
+       aes(x = bill_length_mm, y = flipper_length_mm)) %>% 
+  geom_point()
+
+
+datapasta::df_paste(head(penguins, 5) [, c('bill_length_mm', 'flipper_length_mm')])
+
+## here is my reprex
+
+my_data <- data.frame(
+     bill_length_mm = c(39.1, 39.5, 40.3, NA, 36.7),
+  flipper_length_mm = c(181L, 186L, 195L, NA, 193L)
+)
+
+ggplot(data = my_data,
+       aes(x = bill_length_mm, y = flipper_length_mm)) %>% 
+  geom_point()
+
+
